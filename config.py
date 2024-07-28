037a1569ad55fcc1ff72aa89398ea7c55c1bc07d
Dinah-Ngatia5 <dinah.ngatia@student.moringaschool.com>
2024-07-28 22:44:23 +0300

    Set debug to false

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://dinah5:123456@localhost:5432/lost_and_found_db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///lost_and_found.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://dinah_5:946Vp9ukptGKnMUiOOg3SVsNOHxPMPjD@dpg-cqilr6mehbks73c0996g-a.oregon-postgres.render.com/lost_and_found_x71k')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///lost_and_found.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY') or 'you-will-never-guess'
     SESSION_TYPE = 'filesystem'
     SESSION_COOKIE_SECURE = False
-    DEBUG = True  # Set to False in production
+    DEBUG = False  # Set to False in production
     BCRYPT_LOG_ROUNDS = 12  # Number of hashing rounds
     # Flask-Login settings
     LOGIN_DISABLED = False  # Set to True to disable login
